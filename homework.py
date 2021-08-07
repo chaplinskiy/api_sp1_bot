19a3f5828f40a3b80b1c5ec520fa915477504661
Ivan Chaplinskiy <chaplinskiy@ya.ru>
2021-08-07 23:49:46 +0300

    further url-related mess

--- homework.py
+++ homework.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
-PRAKTIKUM_URL = os.getenv('PRAKTIKUM_URL')
+# PRAKTIKUM_URL = os.getenv('PRAKTIKUM_URL')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
@@ -41,12 +41,11 @@ def parse_homework_status(homework):
 
 def get_homeworks(current_timestamp):
     yptoken = PRAKTIKUM_TOKEN
-    ypurl = PRAKTIKUM_URL
-    url = f'{ypurl}user_api/homework_statuses/'
+    # url = f'{PRAKTIKUM_URL}user_api/homework_statuses/'
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {yptoken}'}
     current_timestamp = current_timestamp or int(time.time())
     payload = {'from_date': current_timestamp}
-    # payload = {'from_date': 0}
     try:
         homework_statuses = requests.get(url, headers=headers, params=payload)
         return homework_statuses.json()
